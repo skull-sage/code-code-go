b8e86cc7baa85edbabb8852f9b933035edd8f694
Muhammad Rashed Alam <rashedalam@Administrators-MacBook-Pro.local>
2025-05-19 21:48:56 +0600

    go's container/list seems not useful much

--- common_ds/trapping_water_test.go
+++ common_ds/trapping_water_test.go
@@ -1,30 +1,54 @@
 package common_ds
 
 import (
-	"fmt"
+	"container/list"
 	"testing"
 )
 
+type Item struct {
+	Val int
+	Idx int
+}
+
+// calculate a relation arr: next idx with equal or greater height of current idx
+func computeIdxBound(hArr []int) []int {
+	stack := list.New()
+	nextIdxBound := make([]int, len(hArr)) // initialize with zeroed value
+	rdx := len(hArr) - 1
+	stack.PushFront(Item{Val: hArr[rdx], Idx: rdx})
+
+	for ldx := rdx - 1; ldx >= 0; ldx-- {
+		lval := hArr[ldx]
+
+		for topItem := stack.Front().Value.(Item); stack.Len() > 0 && topItem.Val < lval; {
+			stack.Remove(stack.Front()) // pop every item less than current value
+			topItem = stack.Front().Value.(Item)
+		}
+		nextIdxBound[ldx] = topItem.Idx
+		stack.PushFront(Item{Val: lval, Idx: ldx})
+	}
+
+	return nextIdxBound
+}
+
 func trap(hArr []int) int {
 	// find the max height
-	right := len(hArr) - 1
-	trapped := 0
-	tmpTrapped := 0
 
-	for left := right; left >= 0; left-- {
-		if hArr[left] >= hArr[right] {
-			fmt.Println("left: ", left, " right: ", right, " tmpTrapped: ", tmpTrapped)
-			right = left
-			trapped += tmpTrapped
-			tmpTrapped = 0
-			fmt.Println("trapped: ", trapped)
-		} else if hArr[left] < hArr[right] {
-			tmpTrapped += hArr[right] - hArr[left]
+	trapped := 0
+	nextIdxBound := computeIdxBound(hArr)
+
+	for ldx := 0; ldx < len(hArr); ldx++ {
+
+		nextIdx := nextIdxBound[ldx]
+
+		for idx := ldx + 1; idx < nextIdx; idx++ {
+			trapped += hArr[ldx] - hArr[idx]
 		}
+		// shift ldx to nextIdx
+		ldx = nextIdx + 1
 	}
 
 	return trapped
-
 }
 
 func TestTrap(t *testing.T) {
