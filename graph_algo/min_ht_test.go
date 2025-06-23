2fb88ab89582c2b6a903d14d405ddaab0a8d0d96
Muhammad Rashed Alam <rashedalam@Administrators-MacBook-Pro.local>
2025-06-23 20:11:53 +0600

    level pruning for minHT seems to require hackish approach

--- graph_algo/min_ht_test.go
+++ graph_algo/min_ht_test.go
@@ -1,7 +1,7 @@
 package graph_algo
 
 import (
-	graph "algo/main/graph"
+	"container/list"
 	"fmt"
 	"testing"
 )
@@ -15,26 +15,37 @@ const (
 // T is the generic type of id::identity for graph nodes
 
 // NOTE: GO is pass by value-copy, you should always use pointer
-type Node[T comparable] struct {
-	Val    T
-	AdjSet map[T]bool
+type Node[K comparable] struct {
+	Val    any
+	Key    K
+	AdjSet map[K]bool
 }
 
-type Graph[T comparable] struct {
-	NodeMap    map[T]*graph.Node[T]
-	visitState map[T]int
+func (self *Node[K]) adjLen() int {
+	return len(self.AdjSet)
 }
 
-func (self *Graph[T]) mapNode(val T) *graph.Node[T] {
-	node, ok := self.NodeMap[val]
-	if !ok {
-		node = &Node[T]{Val: val, AdjSet: map[T]bool{}}
-		self.NodeMap[val] = node
+func (self *Node[K]) String() string {
+	return fmt.Sprintf("Val: %v, AdjSet: %v", self.Val, self.AdjSet)
+}
+
+type Graph[K comparable] struct {
+	NodeMap    map[K]*Node[K]
+	visitState map[K]int
+	extractKey func(any) K
+	directed   bool
+}
+
+func NewGraph[K comparable](directed bool, extractKey func(any) K) *Graph[K] {
+	return &Graph[K]{
+		NodeMap:    make(map[K]*Node[K]),
+		visitState: make(map[K]int),
+		extractKey: extractKey,
+		directed:   directed,
 	}
-	return node
 }
 
-func (self *Graph[T]) String() string {
+func (self *Graph[K]) String() string {
 	str := ""
 	for k, v := range self.NodeMap {
 		str += fmt.Sprintf("%v: %v\n", k, v)
@@ -42,19 +53,45 @@ func (self *Graph[T]) String() string {
 	return str
 }
 
-func (self *Graph[T]) addEdge(u, v T, directed bool) {
-	uNode := self.mapNode(u)
-	vNode := self.mapNode(v)
+func (self *Graph[K]) mapNode(val any, k K) *Node[K] {
 
-	uNode.appendAdj(vNode)
-	if !directed {
-		vNode.appendAdj(uNode)
+	node, ok := self.NodeMap[k]
+	if !ok {
+		node = &Node[K]{Val: val, AdjSet: map[K]bool{}}
+		self.NodeMap[k] = node
+	}
+	return node
+}
+
+func (self *Graph[K]) node(k K) *Node[K] {
+	node, ok := self.NodeMap[k]
+	if !ok {
+		panic("node not found")
+	}
+	return node
+}
+
+func (self *Graph[K]) addEdge(u, v any) {
+	uKey := self.extractKey(u)
+	vKey := self.extractKey(v)
+	uNode := self.mapNode(u, uKey)
+	vNode := self.mapNode(v, vKey)
+	uNode.AdjSet[vKey] = true
+	if !self.directed {
+		vNode.AdjSet[uKey] = true
 	}
 
 }
 
-func NewGraph[T comparable]() *Graph[T] {
-	return &Graph[T]{NodeMap: make(map[T]*Node[T]), visitState: make(map[T]int)}
+func (self *Graph[K]) removeEdge(uKey, vKey K) {
+
+	uNode := self.NodeMap[uKey]
+	vNode := self.NodeMap[vKey]
+
+	delete(uNode.AdjSet, vKey)
+	if !self.directed {
+		delete(vNode.AdjSet, uKey)
+	}
 }
 
 func findMinHeightTrees(n int, edges [][]int) []int {
@@ -63,99 +100,75 @@ func findMinHeightTrees(n int, edges [][]int) []int {
 		return []int{0}
 	}
 
-	graph := NewGraph[int]()
+	graph := NewGraph(false, func(a any) int { return a.(int) })
 
 	for idx := range edges {
 		edge := edges[idx]
-		graph.addEdge(edge[0], edge[1], false)
+		graph.addEdge(edge[0], edge[1])
 	}
 
-	//fmt.Println("# Created Graph: ", graph)
-
-	// inner struct
-	type phi struct {
-		node   *Node[int]
-		parent *Node[int]
-		length int
+	deque := list.New()
+	level := 0
+	for k, v := range graph.NodeMap {
+		if v.adjLen() == 1 {
+			deque.PushBack(k)
+		}
 	}
 
-	// init phi
-	phiArr := make([]phi, n)
-	for idx := range phiArr {
-		phiArr[idx].node = graph.NodeMap[idx]
-		phiArr[idx].parent = nil
-		phiArr[idx].length = 0
-	}
+	remaining := n
 
-	for k := range graph.NodeMap {
-		graph.visitState[k] = NOT_DISCOVERED
-	}
+	for remaining > 2 {
 
-	var dfs func(u *Node[int], phiArr []phi)
-	// inner function : traverse the input tree
-	dfs = func(u *Node[int], phiArr []phi) {
-		graph.visitState[u.Val] = DISCOVERED
+		qLen := deque.Len()
+		remaining = remaining - qLen
 
-		for k, _ := range u.AdjSet {
-			v := graph.NodeMap[k]
-			if graph.visitState[v.Val] == NOT_DISCOVERED {
-				phiArr[v.Val].parent = u
-				phiArr[v.Val].length = phiArr[k].length + 1
-				dfs(v, phiArr)
+		ptr := deque.Front()
+
+		for qLen > 0 {
+			uKey := ptr.Value.(int)
+			u := graph.node(uKey)
+
+			for vKey := range u.AdjSet {
+				adjNode := graph.node(vKey)
+				graph.removeEdge(uKey, vKey)
+				if adjNode.adjLen() == 1 {
+					fmt.Println("Pushing ", vKey)
+					deque.PushBack(vKey)
+				}
 			}
+
+			ptrNext := ptr.Next()
+			deque.Remove(ptr)
+			qLen--
+			ptr = ptrNext
+
 		}
 
-		graph.visitState[u.Val] = VISITED
+		level++
+
+		// fmt.Printf("#level= %d Deque:", level)
+		// for jtr := deque.Front(); jtr != nil; jtr = jtr.Next() {
+		// 	fmt.Printf(" %v", jtr.Value.(int))
+		// }
+		// fmt.Println()
 
 	}
 
-	for _, v := range graph.NodeMap {
-		if len(v.AdjSet) == 1 {
-
-			//fmt.Println("# Start DFS with v: ", v.Val)
-			dfs(v, phiArr)
-			break
-		}
-	}
-
-	// fmt.Println("# Calculated phi: ")
-	// for idx := range phiArr {
-	// 	fmt.Printf("%d->len:%d ", phiArr[idx].node.Val, phiArr[idx].length)
-	// }
-
-	// find the max height
-	var maxPhi phi = phiArr[0]
-	for idx := 1; idx < len(phiArr); idx++ {
-		phi := phiArr[idx]
-		if phi.length > maxPhi.length {
-			maxPhi = phi
-		}
-	}
-
-	//fmt.Printf("\n# Max phi: %d->len:%d\n", maxPhi.node.Val, maxPhi.length)
-
-	traceBack := maxPhi.length / 2
-	aPhi := maxPhi
-	for idx := traceBack; idx >= 1; idx-- {
-		aPhi = phiArr[aPhi.parent.Val]
-		//fmt.Printf("%d->len:%d ", aPhi.node.Val, aPhi.length)
-	}
-
-	//fmt.Println()
-	// total number of nodes = maxPhi.length + 1
-	if (maxPhi.length+1)%2 == 0 {
-		return []int{aPhi.node.Val, aPhi.parent.Val}
+	if remaining == 2 {
+		return []int{deque.Front().Value.(int), deque.Back().Value.(int)}
 	} else {
-		return []int{aPhi.node.Val}
+		return []int{deque.Front().Value.(int)}
 	}
+
 }
 
 func TestMinHT(t *testing.T) {
-	n := 2
-	edges := [][]int{{0, 1}}
+	//n := 7
+	//edges := [][]int{{3, 0}, {3, 1}, {3, 2}, {3, 4}, {5, 4}}
+
+	n := 3
+	edges := [][]int{{1, 0}, {1, 2}}
+
 	roots := findMinHeightTrees(n, edges)
 	fmt.Println("#roots", roots)
-	// edges := [][]int{{3, 0}, {3, 1}, {3, 2}, {3, 4}, {5, 4}, {5, 6}}
-	// roots := findMinHeightTrees(n, edges)
-	// fmt.Println("#roots", roots)
 }
