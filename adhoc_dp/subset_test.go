2437306635bbcc1996898171c5cb024a11464635
Muhammad Rashed Alam <rashedalam@Administrators-MacBook-Pro.local>
2025-08-03 21:22:39 +0600

    subset completed

--- adhoc_dp/subset_test.go
+++ adhoc_dp/subset_test.go
@@ -1,23 +1,45 @@
 package adhoc_dp
 
-type ResultArr [][]int
+import (
+	"sort"
+	"testing"
+)
 
-func addToResult(curr *[]int)
+type ResultArr [][]int
 
 func recurSubset(idx int, curr []int, nums []int, result *ResultArr) {
 
 	tmp := make([]int, len(curr))
 	copy(tmp, curr)
+	*result = append(*result, tmp)
+
+	l := len(curr)
 
 	for i := idx + 1; i < len(nums); i++ {
 
-		curr = append(curr, nums[i])
+		if l > 0 && nums[i] <= curr[l-1] {
+			continue
+		}
+
+		curr = append(curr, nums[i]) // {1} / {2} / {3} / {4}
+		//fmt.Println("idx:", idx, "i:", i, curr)
 		recurSubset(idx+1, curr, nums, result)
-		curr = curr[:len(curr)-1]
+		curr = curr[:l]
 	}
 
 }
 
 func subsets(nums []int) [][]int {
+	sort.Ints(nums)
+	ans := make(ResultArr, 0)
+	curr := make([]int, 0)
+	recurSubset(-1, curr, nums, &ans)
+	return ans
+}
 
+func TestSubset(t *testing.T) {
+
+	nums := []int{1, 2, 3, 4}
+	ans := subsets(nums)
+	t.Log(ans)
 }
