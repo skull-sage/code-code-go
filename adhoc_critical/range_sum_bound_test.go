d85644cbd2d1b2440fd40d82ef6907acad506f8d
Rashed Alam <rashed091@gmail.com>
2025-08-19 03:10:36 +0600

    why prefix sum first index needed to be 0 ?

--- adhoc_critical/range_sum_bound_test.go
+++ adhoc_critical/range_sum_bound_test.go
@@ -1,5 +1,10 @@
 package adhoc_critical
 
+import (
+	"fmt"
+	"testing"
+)
+
 type Input struct {
 	pfSum []int
 	lower int
@@ -7,82 +12,107 @@ type Input struct {
 	count int
 }
 
-func (this *Input) sortAndCount(low, high int) {
+func (this *Input) sortAndCount(low, high, level int) {
 
 	if low >= high {
 		return
 	}
 
-	mid := (high + low) / 2 // l = 2, h = 7, m = 4
+	mid := low + (high-low)/2 // l = 2, h = 7, m = 4
 
-	this.sortAndCount(low, mid)
-	this.sortAndCount(mid, high)
+	this.sortAndCount(low, mid, level+1)
+	this.sortAndCount(mid+1, high, level+1)
 
-	arr := this.pfSum
-
-	i := mid
-	j := mid
+	i := mid + 1
+	j := mid + 1
 	// a subset sum =>  ---arr[k]{-----}arr[i]----
-	for k := 0; k < mid; k++ {
-		for i < high && (arr[i]-arr[k]) < this.lower {
+	for k := low; k <= mid; k++ {
+		for i <= high && (this.pfSum[i]-this.pfSum[k]) < this.lower {
 			i++
 		}
 
-		for j < high && (arr[j]-arr[k]) <= this.upper {
+		//j = i
+		//fmt.Printf("j=%d, k=%d %d", j, k, (this.pfSum[j] - this.pfSum[k]))
+		for j <= high && (this.pfSum[j]-this.pfSum[k]) <= this.upper {
+			//fmt.Printf("j=%d, k=%d %d", j, k, (this.pfSum[j] - this.pfSum[k]))
 			j++
 		}
 
 	}
 
+	//fmt.Println(low, mid, high, "j", j, "i", i)
 	this.count += (j - i)
-	this.merge(low, mid, high)
+	this.merge(low, mid, high, level)
 
 }
 
-func (this *Input) merge(low, mid, high int) {
+func (this *Input) merge(low, mid, high, level int) {
 	//merge step
 
-	lowEnd := this.pfSum[low:mid]
-	highEnd := this.pfSum[mid:high]
+	left := make([]int, (mid + 1 - low)) //this.pfSum[low : mid+1]
+	copy(left, this.pfSum[low:mid+1])
 
+	right := make([]int, (high - mid))
+	copy(right, this.pfSum[mid+1:high+1])
+
+	fmt.Println("level", level, "l:", low, "m:", mid, "h:", high, "=> left", left, "right", right)
 	i := 0
 	j := 0
 	k := 0
-	for i < mid && j < high {
-		if lowEnd[i] <= highEnd[j] {
-			this.pfSum[k] = lowEnd[i]
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			this.pfSum[k] = left[i]
 			k++
 			i++
 		} else {
-			this.pfSum[k] = highEnd[j]
+			this.pfSum[k] = right[j]
 			k++
 			j++
 		}
+
+		fmt.Println("# merging: ", i, j, k, this.pfSum)
 	}
 
-	for i < mid {
-		this.pfSum[k] = lowEnd[i]
+	for i < len(left) {
+		this.pfSum[k] = left[i]
 		k++
 		i++
+
 	}
 
-	for j < high {
-		this.pfSum[k] = highEnd[j]
+	fmt.Println("# merging: ", i, j, k, this.pfSum)
+
+	for j < len(right) {
+		this.pfSum[k] = right[j]
 		k++
 		j++
 	}
+
+	fmt.Println("# after merging:", this.pfSum)
 }
 
 func countRangeSum(nums []int, lower int, upper int) int {
 
 	pfSum := make([]int, len(nums))
 
-	for idx := 0; idx < len(nums); idx++ {
+	pfSum[0] = nums[0]
+
+	for idx := 1; idx < len(nums); idx++ {
 		pfSum[idx] = pfSum[idx-1] + nums[idx]
 	}
 
+	fmt.Println("before sorting", pfSum)
 	input := Input{pfSum, lower, upper, 0}
-	input.sortAndCount(0, len(pfSum))
+	input.sortAndCount(0, len(pfSum)-1, 0)
+	fmt.Println("after sorting", input.pfSum)
 	return input.count
 
+}
+
+func TestRangeCount(t *testing.T) {
+	nums := []int{-2, 5, -1}
+	lower := -2
+	upper := 5
+
+	fmt.Println(countRangeSum(nums, lower, upper))
 }
