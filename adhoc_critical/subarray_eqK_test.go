08b53c37b7474af3f553ad236e4a3f704fdfe8db
Rashed Alam <rashed091@gmail.com>
2025-08-21 03:34:20 +0600

    it is time to delving into basics

---
+++ adhoc_critical/subarray_eqK_test.go
@@ -0,0 +1,37 @@
+package adhoc_critical
+
+import (
+	"fmt"
+	"sort"
+)
+
+func subarraySum(nums []int, k int) int {
+	idxListMap := make(map[int][]int, 0)
+
+	psum := make([]int, len(nums)+1)
+	psum[0] = 0
+	for idx := 1; idx < len(psum); idx++ {
+		sum := psum[idx-1] + nums[idx-1]
+		psum[idx] = sum
+		if idxListMap[sum] == nil {
+			idxListMap[sum] = make([]int, 0)
+		}
+		idxListMap[sum] = append(idxListMap[sum], idx)
+	}
+
+	fmt.Println(psum)
+	fmt.Println(idxListMap)
+
+	result := 0
+	for idx := 0; idx < len(psum); idx++ {
+
+		target := psum[idx] + k
+		list := idxListMap[target]
+
+		idxStart := sort.SearchInts(list, idx)
+		result += (len(list) - idxStart)
+	}
+
+	return result
+
+}
