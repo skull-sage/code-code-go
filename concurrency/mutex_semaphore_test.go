71b67dbc7feb28e93b87f7de8c019c1c70a45e77
Rashed Alam <rashed091@gmail.com>
2025-09-11 05:10:23 +0600

    semaphore struct created

--- concurrency/mutex_semaphore_test.go
+++ concurrency/mutex_semaphore_test.go
@@ -6,32 +6,46 @@ import (
 	"time"
 )
 
+type Semaphore struct {
+	signalBuff chan struct{}
+}
+
+func (sb *Semaphore) acquire() {
+	sb.signalBuff <- struct{}{}
+}
+
+func (sb *Semaphore) release() {
+	<-sb.signalBuff
+}
+
 func TestSemaphor(t *testing.T) {
 	// buffered channel as a semaphore blocking mechanism
 
-	signalBuff := make(chan bool, 3)
+	semaphore := Semaphore{signalBuff: make(chan struct{}, 3)}
 
 	// worker will recieve it and send it
 	// a recive must follow  a send:
-	var worker func(wid, counter int) = func(wid, counter int) {
+	var worker func(counter int) = func(counter int) {
 		// sending to buffer gets blocked only after buff is full
-		signalBuff <- true
-		// critical section start
-		time.Sleep(3 * time.Second)
-		fmt.Println("Routine id:", wid, "Printing Counter: ", counter)
-
-		//critical section ends
-
-		<-signalBuff // receiving
+		sleepTime := (counter % 3) + 2
+		time.Sleep(time.Duration(sleepTime) * time.Second)
+		fmt.Println("Printing Counter: ", counter)
 
 	}
 
 	fmt.Println("Semaphore with capacity 3")
-	counter := 0
-	for id := range 10 {
-		counter++
-		go worker(id, counter)
-		counter = counter % cap(signalBuff)
+	for counter := range 10 {
+
+		semaphore.acquire()
+		// critical section start we want 3 worker to work at a time
+		go worker(counter)
+		//critical section ends
+		if counter%3 == 0 {
+			fmt.Println()
+		}
+		semaphore.release() // receiving
+
+		counter = counter % cap(semaphore.signalBuff)
 	}
 
 	// give other routine sometime to work before exiting
